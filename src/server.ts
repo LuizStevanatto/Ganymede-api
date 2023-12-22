61378c409714aef5cddfd961ccf481642d24fee9
Luiz Stevanatto <luiz.stevanatto5@gmail.com>
2023-12-22 09:29:57 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
 import "reflect-metadata";
 import * as dotenv from "dotenv";
+import * as os from "os";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3333;
@@ -8,5 +10,8 @@ const PORT = process.env.PORT || 3333;
 import "./database/connection";
 
 app.listen(PORT, () => {
+	const networkInfo = os.networkInterfaces();
+	console.log(networkInfo); // objeto
+	console.log(networkInfo.Ethernet[0].address); // ip
 	console.log(`Server is running on PORT:${PORT}`);
 });
