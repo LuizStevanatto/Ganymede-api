37db97e8d38d019d2548fc0f043372b9a8ceea17
Luiz Stevanatto <luiz.stevanatto5@gmail.com>
2023-12-22 09:36:49 -0300

    up

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { chcRouter } from "./routes/chc.routes";
 import { nccAssRouter } from "./routes/nccAss.route";
 import { nccLtdaRouter } from "./routes/nccLtda.route";
 import { tlabRouter } from "./routes/tlab.route";
+import * as os from "os";
 
 const router = Router();
 
@@ -12,6 +13,8 @@ router.use("/nccLtda", nccLtdaRouter);
 router.use("/tlab", tlabRouter);
 
 router.get("/", async (request, response) => {
+	const networkInfo = os.networkInterfaces();
+	console.log(networkInfo.Ethernet[0].address); // ip
 	console.log("APIII");
 
 	return response.status(200).send({ message: "Main Route Ganymede" });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { app } from "./app";
 import "reflect-metadata";
 import * as dotenv from "dotenv";
-import * as os from "os";
 
 dotenv.config();
 
@@ -10,8 +9,5 @@ const PORT = process.env.PORT || 3333;
 import "./database/connection";
 
 app.listen(PORT, () => {
-	const networkInfo = os.networkInterfaces();
-	console.log(networkInfo); // objeto
-	console.log(networkInfo.Ethernet[0].address); // ip
 	console.log(`Server is running on PORT:${PORT}`);
 });
