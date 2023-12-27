b1b260f7b1fd2f7a553f7de246be01ab2b7d56ad
Luiz Stevanatto <luiz.stevanatto5@gmail.com>
2023-12-27 15:14:18 -0300

    stable version for fluig auth

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3454;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT:${PORT}`);
